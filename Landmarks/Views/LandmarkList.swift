f49db1a549d936254524989b7218ebb249ab52d8
Shogo <vanitas710@yahoo.co.jp>
2021-03-05 03:00:46 +0900

    リストを使ってランドマークを全て表示した

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Shogo Nobuhara on 2021/03/05.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
