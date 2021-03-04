ad64a75af990a431270a0fece14055685a55a1e2
Shogo <vanitas710@yahoo.co.jp>
2021-03-05 03:13:53 +0900

    gitの管理対象から外れたので、追加した

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,40 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                // マップコンテンツを上端(ノッチ)まで拡張する
-                .ignoresSafeArea(edges:.top)
-                .frame(height: 300)
-            
-            CircleImage()
-                // オフセットで画像をずらした分、パディングで上げる
-                .offset(y: -130)
-                .padding(.bottom,-130)
-            
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                HStack {
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("California")
-                        .font(.subheadline)
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
-                Divider()
-                
-                Text("about Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-            }
-            .padding()
-            
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,53 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Shogo Nobuhara on 2021/03/05.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                // マップコンテンツを上端(ノッチ)まで拡張する
+                .ignoresSafeArea(edges:.top)
+                .frame(height: 300)
+            
+            CircleImage()
+                // オフセットで画像をずらした分、パディングで上げる
+                .offset(y: -130)
+                .padding(.bottom,-130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("about Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
