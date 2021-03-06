62cf90e32f016793123bccaace561d042a4ec544
Shogo <vanitas710@yahoo.co.jp>
2021-03-07 03:53:28 +0900

    お気に入りボタンを追加して、お気に入りした奴のみ表示できるようにした

---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,51 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Shogo Nobuhara on 2021/03/05.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    // フィルタされたものだけこの変数に格納される
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                // フィルタに格納された数だけループする
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink(
+                        destination: LandmarkDetail(landmark:  landmark)){
+                        LandmarkRow(landmark:  landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPod touch (7th generation)","iPhone 12 Pro Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .environmentObject(ModelData())
+                //.previewDevice(PreviewDevice(rawValue: deviceName))
+                //.previewDisplayName(deviceName)
+        }
+    }
+}
